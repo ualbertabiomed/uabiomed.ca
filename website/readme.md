52eddc5929eec864c5a59f064faa4fcc2c5d0a04
Jacob Reckhard <jacobreckhard@gmail.com>
2018-09-07 11:20:15 -0600

    inital commit

---
+++ website/readme.md
@@ -0,0 +1,13 @@
+advartise info
+collect join info
+applying for positions
+sponsors contact
+open positions (tree diagram, generated)
+
+
+
+Server:
+mailing list
+sql database of members
+Member management program
+
---
+++ website/server/serve.py
@@ -0,0 +1,33 @@
+import tornado.ioloop
+import tornado.web
+
+import os
+
+
+class StaticHandler(tornado.web.StaticFileHandler):
+    def parse_url_path(self, url_path):
+        if not url_path or url_path.endswith('/'):
+            url_path = url_path + 'index.html'
+        if not '.' in url_path:
+            url_path = url_path + '.html'
+        return url_path
+
+class JoinHandler(tornado.web.RequestHandler):
+    def post(self):
+        memb = {}
+        for x in ['name', 'email', 'msg']:
+            memb[x] = self.get_body_argument(x, default=None, strip=False)
+        print(memb)
+        self.write('<!doctype html><meta charset=utf-8><title>redirect</title><meta http-equiv="Refresh" content="0; url=/">')
+
+application = tornado.web.Application([
+    (r"/iamanewmember", JoinHandler),
+    (r"/(.*)", StaticHandler, {"path": os.getcwd() + "/site/bin"})
+], debug=True)
+
+try:
+    print("server starting")
+    application.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
+except KeyboardInterrupt:
+    print("server exited")
