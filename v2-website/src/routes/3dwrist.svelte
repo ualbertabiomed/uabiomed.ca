fb293462673956d084e7b9a6f4996ad51af4a731
Justin Boileau <jboileau@ualberta.ca>
2021-04-02 13:01:38 -0600

    Added 3d wrist info

--- v2-website/src/routes/3dwrist.svelte
+++ v2-website/src/routes/3dwrist.svelte
@@ -25,24 +25,21 @@
     <h2> 3D Wrist Project </h2>
     <div class="hor">
         <p>
-            Words
-        <p>
+            The objective of the 3D Wrist Project is to design and develop a 3D-printed synthetic wrist model, composed of synthetic bone and ligaments with similar anatomical and biomechanical properties as those of a human wrist. The developed 3D printed wrist model will be used for orthopedic biomechanics research; specifically, the team is working on developing a four-corner fusion model which is a common surgical procedure performed to alleviate wrist pain and improve grip strength, all while preserving some range of motion in patients with degenerative conditions of the wrist. In the future, models of other common fusing techniques will be developed for comparison of various metrics between models to evaluate different fusing techniques, ultimately to try and improve clinical outcomes of patients with degenerative conditions of the wrist.<br><br>
+
+            In Winter 2021, the team developed initial SolidWorks CAD models of the wrist bones and ligaments. As well, a flowerplate model is being developed for the wrist model as it is a common tool used to connect carpal bones in the 4-corner fusion procedure. ABS or PLA 3D printing material will be used for the carpal bones to produce material properties similar to healthy human bones, while a pliable material such as Ninja- or Poly-flex will be used to 3D print the wrist ligaments to simulate their biomechanical properties.
+        </p>
 
         <div>
-            <img src="/images/elbow-real.png" class="img1">
+            <img src="/images/3dwrist.png" class="img1">
         </div>
     </div>
 
     <div class="hor">
-        <div>
-            <img src="/images/elbow-fake.png" class="img2">
-        </div>
         <p>
-            Words
-        </p>
+            The team has also been designing the supporting structure/actuator system (“test rig”) for generating and controlling flexion/extension of the wrist model. The team aims to significantly reduce costs of the test rig compared to other similar structures found in relevant literature by designing and 3D printing much of the necessary hardware in-house and utilizing  a system of pulleys, cables and gears to minimize the number of motors required for controlling the wrist model motion. A gear-based single-motor model and a self-supporting pulley-based model are two preliminary designs that the team plans to further design, build and test in Summer and Fall 2021.<br><br>
 
+            The team looks forward to using Summer and Fall 2021 to begin initial 3D prints of the wrist model and test rig hardware for preliminary testing and design iteration, leaving a broad range of opportunities for students to get involved with CAD modeling, 3D printing, component and system testing, instrumentation, wrist kinematics and data collection/analysis, and more!            
+        </p>
     </div>
-    <p>
-        Words
-    </p>
 </div>
