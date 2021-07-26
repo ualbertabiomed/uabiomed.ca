0f33ac6b3b56931c4ddaa74d10a06c9ec2abc033
AlphonsoD <alphonso@ualberta.ca>
2021-07-26 11:30:22 -0600

    revert sidebar changes

--- v2-website/src/components/sidebar.svelte
+++ v2-website/src/components/sidebar.svelte
@@ -6,25 +6,9 @@
                 <h2>Menu</h2>
             </header>
             <ul>
-                <!--{#each links as link}
+                {#each links as link}
                     <li><a on:click={()=>setSidebar(false)} href={link.loc}>{link.name}</a></li>
                 {/each}
-                -->
-                <li><a on:click={()=>setSidebar(false)} href={'/'}>About Us</a></li>
-                <li><a on:click={()=>setSidebar(false)} href={'../routes/exoskeleton.svelte'}>ALEX - The Exoskeleton</a></li>
-                <li><a on:click={()=>setSidebar(false)} href={'/3dwrist'}>3D Wrist Project</a></li>
-                <li><a on:click={()=>setSidebar(false)} href={'/team'}>The Team</a></li>
-                <li><a on:click={()=>setSidebar(false)} href={'/info'}>Why Join a Biomedical Engineering Group? (Non-STEM students)</a></li>
-                <li><a on:click={()=>setSidebar(false)} href={'/join'}>Join the Team</a></li>
-                <li><a on:click={()=>setSidebar(false)} href={'/press'}>Press and Educational Outreach</a></li>
-                <li><a on:click={()=>setSidebar(false)} href={'/contact'}>Contact Us</a></li>
-                <li>
-                    <span class="opener">Test Submenu</span>
-                    <ul>
-                        <li><a on:click={()=>setSidebar(false)} href={'/contact'}>Test 1 (contact)</a></li>
-                        <li><a on:click={()=>setSidebar(false)} href={'/team'}>Test 2 (team)</a></li>
-                    </ul>
-                </li>
             </ul>
         </nav>
         <footer id="footer">
