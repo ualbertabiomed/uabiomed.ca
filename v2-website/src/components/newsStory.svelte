eb5581f6fe3bcf568d0eae252b3af690512b6ffa
AlphonsoD <alphonso@ualberta.ca>
2021-09-06 20:01:51 -0600

    adjusted news margins

--- v2-website/src/components/newsStory.svelte
+++ v2-website/src/components/newsStory.svelte
@@ -1,4 +1,4 @@
-<div>
+<div class="news">
     <h3>{source}</h3>
     <p>{blurb}</p>
     <a href={link}>Read more here</a>
@@ -6,6 +6,11 @@
 </div>
 
 <style>
+    .news {
+        width: 80%;
+        margin-left: 10%;
+        margin-top: 50px;
+    }
     p {
         margin-bottom: 0;
     }
